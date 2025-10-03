ff5a0a435870dd807f87c6339deb8667856aba22
Annikahlaursen <Annikahlaursen@gmail.com>
2025-10-03 10:12:16 +0200

    Add greeting message to ContactPage component

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,9 +1,10 @@
 export default function ContactPage() {
-    return (
-        <section className="page">
-            <h1>Contact Page</h1>
-            <p>Get in touch, or swing by for a cup of coffee ☕️</p>
-            <p>I am one arm away 🤷🏼‍♂️</p>
-        </section>
-    );
+  return (
+    <section className="page">
+      <h1>Contact Page</h1>
+      <p>Get in touch, or swing by for a cup of coffee ☕️</p>
+      <p>I am one arm away 🤷🏼‍♂️</p>
+      <p>Hej</p>
+    </section>
+  );
 }
