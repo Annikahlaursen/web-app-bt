7fb4f0206add4173d512aa7e59c7bda576099634
Annikahlaursen <Annikahlaursen@gmail.com>
2025-10-03 10:07:13 +0200

    Add additional paragraph to HomePage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
 export default function HomePage() {
-    return (
-        <section className="page">
-            <h1>Home Page</h1>
-            <p>Home is where the heart is 💛</p>
-            <p>Oh My, sounds like a bad movie!</p>
-        </section>
-    );
+  return (
+    <section className="page">
+      <h1>Home Page</h1>
+      <p>Home is where the heart is 💛</p>
+      <p>Oh My, sounds like a bad movie!</p>
+      <p>Hej ændringer</p>
+    </section>
+  );
 }
