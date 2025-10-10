2e9c6825fe1797e5f47ce208d97bdc0c7ec63bad
Annikahlaursen <Annikahlaursen@gmail.com>
2025-10-10 11:03:12 +0200

    Add Font Awesome icons to navigation links and update dependencies

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,18 @@
 import { NavLink } from "react-router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faHome, faCalendar } from "@fortawesome/free-solid-svg-icons";
 
 export default function Nav() {
   return (
     <nav>
       <NavLink className="navcircle" to="/">
-        Home
+        <FontAwesomeIcon icon={faHome} />
       </NavLink>
       <NavLink className="navcircle" to="/about">
-        About
+        <FontAwesomeIcon icon={faCalendar} />
       </NavLink>
       <NavLink className="navcircle" to="/contact">
-        Contact
+        <FontAwesomeIcon icon={faBars} />
       </NavLink>
     </nav>
   );
