783c18d64d5773eff07c5f751186954c035b5e36
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-10-13 11:17:01 +0200

    add styling and icons to KampPage component; enhance layout with new CSS classes and SVG assets

--- src/pages/KampPage.jsx
+++ src/pages/KampPage.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router";
 import VSVisning from "../components/VSVisning";
 import arrowWhite from "/public/arrow-left-white.svg";
+import clock from "/public/clock.svg";
+import calendar from "/public/calendar-outline.svg";
+import location from "/public/location-dot.svg";
+import tableTennis from "/public/table-tennis-icon.svg";
 
 export default function KampPage() {
   return (
@@ -17,21 +21,24 @@ export default function KampPage() {
       </div>
       <section className="kamp-info-section">
         <div className="kamp-info">
-          <img src="" alt="" />
+          <img src={tableTennis} alt="TableTennis icon" />
           <p>Kamp ID</p>
         </div>
         <div className="kamp-info">
-          <img src="" alt="" />
+          <img src={calendar} alt="Calendar icon" />
           <p>dato oktober 2025</p>
         </div>
         <div className="kamp-info">
-          <img src="" alt="" />
+          <img src={clock} alt="Clock icon" />
           <p>Klokslæt (20:00)</p>
         </div>
         <div className="kamp-info">
-          <img src="" alt="" />
+          <img src={location} alt="Location pin icon" />
           <p>Adresse, postnummer</p>
         </div>
+        <br />
+        <p>Kamp resultat: Afventer</p>
+        <p>Spillere: Afventer</p>
       </section>
     </>
   );
