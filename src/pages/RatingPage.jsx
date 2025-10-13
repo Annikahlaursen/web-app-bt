89d4ac9769aaec8e6e2053cef6f7894b27af22b6
stinelock <eaa24stlo@students.eaaa.dk>
2025-10-13 13:47:06 +0200

    feat: add search functionality to RatingPage

--- src/pages/RatingPage.jsx
+++ src/pages/RatingPage.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import RatingListe from "../components/RatingListe";
 
 export default function RatingPage() {
+    const [searchTerm, setSearchTerm] = useState("");
   return (
     <section className="page">
       <h1>Rating Page</h1>
+
+      <input
+        type="text"
+        placeholder="🔍 Søg i posts..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        style={{ flex: 1, padding: "10px" }}
+      />
       <RatingListe />
     </section>
   );
