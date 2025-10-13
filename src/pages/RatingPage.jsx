561e328965613535043165384a0bb48e9a6d9b42
stinelock <eaa24stlo@students.eaaa.dk>
2025-10-13 14:01:47 +0200

    feat: add styling and search input to RatingPage

--- src/pages/RatingPage.jsx
+++ src/pages/RatingPage.jsx
@@ -3,17 +3,30 @@ import RatingListe from "../components/RatingListe";
 
 export default function RatingPage() {
     const [searchTerm, setSearchTerm] = useState("");
+
+// const filteredPosts = posts.filter((post) => {
+//   const matchesSearch =
+//     post.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+//     post.body.toLowerCase().includes(searchTerm.toLowerCase());
+//   const matchesUser =
+//     selectedUserId === "" || post.userId.toString() === selectedUserId;
+
+//   return matchesSearch && matchesUser;
+// });
+
   return (
     <section className="page">
-      <h1>Rating Page</h1>
-
-      <input
-        type="text"
-        placeholder="🔍 Søg i posts..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        style={{ flex: 1, padding: "10px" }}
-      />
+      <h1>Rating</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Søg i rating"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          style={{ flex: 1, padding: "10px" }}
+        />
+        
+      </div>
       <RatingListe />
     </section>
   );
