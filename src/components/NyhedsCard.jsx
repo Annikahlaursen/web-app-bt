528e4da77f84675de82481a9ee5e3a9942f230fd
stinelock <eaa24stlo@students.eaaa.dk>
2025-10-13 13:18:01 +0200

    feat: add NyhedsCard component and integrate into HomePage with styling

---
+++ src/components/NyhedsCard.jsx
@@ -0,0 +1,13 @@
+export default function NyhedsCard() {
+    return (
+      <article className="nyheds-card">
+        <div className="img-container">
+          <img src="" alt="nyhedsbillede" />
+        </div>
+        <h2>Nyheds Card</h2>
+        <p>
+          Her kommer nyheds card indholdet
+        </p>
+      </article>
+    );
+}
