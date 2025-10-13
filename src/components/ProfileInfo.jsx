e87d8075779412d28791dbbc64e92931cb61d27b
Annikahlaursen <Annikahlaursen@gmail.com>
2025-10-13 13:59:52 +0200

    feat: implement Profile component with profile info and parent views; add SVG icons for user and users

---
+++ src/components/ProfileInfo.jsx
@@ -0,0 +1,80 @@
+import { Fragment } from "react";
+import Image from "../assets/icons/image-solid-full.svg";
+import Pen from "../assets/icons/pen-solid-full.svg";
+import Trash from "../assets/icons/trash-solid-full.svg";
+
+export default function ProfileInfo() {
+  return (
+    <Fragment>
+      <div className="profile-info-parent">
+        <div>
+          <div className="profile-card-header">
+            <h3>Profilbillede</h3>
+          </div>
+          <div className="profile-card-content">
+            <img src={Image} alt="Placeholder image" />
+          </div>
+          <div className="profile-card-actions">
+            <button className="invite-parent">
+              <img src={Pen} alt="Edit icon" />
+              Rediger
+            </button>
+            <button className="plus-parent">
+              <img src={Trash} alt="Delete icon" />
+              Fjern
+            </button>
+          </div>
+        </div>
+
+        <div>
+          <div className="profile-card-header">
+            <h3>Kontaktperson for</h3>
+          </div>
+          <div className="profile-card-content">
+            <form action="ProfileInfo" className="profile-form">
+              <input type="text" id="name" name="name" placeholder="Fornavn" />
+              <input
+                type="text"
+                id="lastname"
+                name="lastname"
+                placeholder="Efternavn"
+              />
+              <input type="text" id="gender" name="gender" placeholder="Køn" />
+              <input type="date" id="birthday" name="Fødselsdato" />
+              <input
+                type="text"
+                id="adress"
+                name="adress"
+                placeholder="Adresse"
+              />
+              <input type="text" id="city" name="city" placeholder="By" />
+              <input type="text" id="zip" name="zip" placeholder="Postnummer" />
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Din email"
+              />
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                placeholder="Dit telefonnummer"
+              />
+            </form>
+          </div>
+          <div className="profile-info-actions">
+            <button className="plus-parent">Gem</button>
+            <br />
+            <button className="plus-parent">Log ud</button>
+            <button className="plus-parent">Opdater adgangskode</button>
+            <button className="plus-parent">Tilknyt login til Face ID</button>
+            <button className="plus-parent">Indstillinger</button>
+            <br />
+            <button className="plus-parent">Slet profil</button>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,12 +1,59 @@
-import User from "../assets/icons/user-solid-full.svg";
+import { useState } from "react";
+import Placeholder from "../assets/icons/image-solid-full.svg";
+import ProfilePic from "../assets/icons/user-solid-full.svg";
+import Users from "../assets/icons/users-solid-full.svg";
+import ProfileInfo from "../components/ProfileInfo";
+import ProfileParent from "../components/ProfileParent";
 
 export default function Profile() {
+  const [activeView, setActiveView] = useState("info"); // 'info' eller 'kontakt'
   return (
-    <div>
+    <div className="profile">
       <div>
-        <img src={User} alt="" />
+        <div className="profile-placeholder">
+          <img src={Placeholder} alt="Placeholder image" />
+        </div>
+        <div className="profile-text">
+          <h4>Heidi Astrup</h4>
+          <h4>Spiller ID:</h4>
+          <h4>Rating:</h4>
+        </div>
       </div>
-      <h1>Profile Page</h1>
+      <div>
+        <div className="profile-btns">
+          <button
+            className={`profile-btns-seperate ${
+              activeView === "info" ? "active" : ""
+            }`}
+            onClick={() => setActiveView("info")}
+          >
+            <img
+              src={ProfilePic}
+              alt="Profile icon placeholder"
+              style={{ width: "2em" }}
+            />
+            <h5>Mine oplysninger</h5>
+          </button>
+          <button
+            className={`profile-btns-seperate ${
+              activeView === "kontakt" ? "active" : ""
+            }`}
+            onClick={() => setActiveView("kontakt")}
+          >
+            <img
+              src={Users}
+              alt="Group icon placeholder"
+              style={{ width: "2em" }}
+            />
+            <h5>Kontaktpersoner</h5>
+          </button>
+        </div>
+      </div>
+
+      {/* Conditional rendering af komponenter */}
+      <div className="profile-content">
+        {activeView === "info" ? <ProfileInfo /> : <ProfileParent />}
+      </div>
     </div>
   );
 }
