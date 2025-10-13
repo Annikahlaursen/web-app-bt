dbb6407e71218f7c8bde95b187922794670959a6
stinelock <eaa24stlo@students.eaaa.dk>
2025-10-13 11:02:53 +0200

    feat: refactor RatingBoks to accept user props and add RatingListe component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
 import KampCard from "../components/KampCard";
-import RatingBoks from "../components/RatingBoks";
 import HoldBoks from "../components/HoldBoks";
+import RatingListe from "../components/RatingListe";
 
 
 export default function HomePage() {
   return (
     <section className="page">
       <HoldBoks />
-      <RatingBoks/>
+      <RatingListe/>
       <h1>Home Page</h1>
       <p>Home is where the heart is 💛</p>
       <p>Oh My, sounds like a bad movie!</p>
