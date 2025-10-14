de2073a693593e0d5056bd4578166382814a5e75
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-10-14 15:09:41 +0200

    feat: implement user fetching in SearchSpiller and update filtering logic; adjust search input label

--- src/components/SearchSpiller.jsx
+++ src/components/SearchSpiller.jsx
@@ -1,31 +1,59 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import RatingBoks from "./RatingBoks";
 
 export default function SearchSpiller() {
   const [searchQuery, setSearchQuery] = useState(""); // set the initial state to an empty string
+  const [users, setUsers] = useState([]); // set the initial state to an empty array
+  const [showResults, setShowResults] = useState(false); // Track input focus
+
+  // Fetch data from the API
+  useEffect(() => {
+    async function fetchUsers() {
+      const response = await fetch(
+        `${import.meta.env.VITE_FIREBASE_DATABASE_URL}/users.json`
+      ); // fetch data from the url
+      const data = await response.json(); // get the data from the response and parse it
+      // from object to array
+      const userArray = Object.keys(data).map((userId) => ({
+        id: userId,
+        ...data[userId],
+      })); // map the data to an array of objects
+
+      setUsers(userArray); // set the posts state with the postsArray
+    }
+
+    fetchUsers();
+  }, []);
+
+  // Only users with hid: -HnAd4o6AtIlkW2SCf6R
+  const teamHid = "-HnAd4o6AtIlkW2SCf6R";
+  const teamUsers = users.filter((user) => user.hid === teamHid);
 
   // Filter posts based on the search query
-  const filteredPosts = posts.filter((post) =>
-    post.caption.toLowerCase().includes(searchQuery)
+  const filteredUsers = teamUsers.filter((user) =>
+    (user.fornavn ?? "").toLowerCase().includes(searchQuery)
   );
   return (
     <>
       <label>
-        Search by caption{" "}
+        Search by name{" "}
         <input
           aria-label="Search by caption"
           defaultValue={searchQuery}
+          onClick={() => setShowResults(true)}
           onChange={(event) => setSearchQuery(event.target.value.toLowerCase())}
           placeholder="Søg spiller"
           type="search"
           name="searchQuery"
         />
       </label>
-      <div className="grid">
-        {filteredPosts.map((post) => (
-          <RatingBoks key={post.id} post={post} />
-        ))}
-      </div>
+      {showResults && (
+        <div>
+          {filteredUsers.map((user, idx) => (
+            <RatingBoks key={user.id} user={user} placering={idx + 1} />
+          ))}
+        </div>
+      )}
     </>
   );
 }
