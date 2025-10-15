583d93ae869016f5461ef1cbcb36801ee6d4b786
Annikahlaursen <Annikahlaursen@gmail.com>
2025-10-15 10:55:59 +0200

    json, app, logincard, profileinfo, firebase-config, signuppage, signincard

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { Routes, Route, Navigate, useLocation } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import { useState, useEffect } from "react";
 import Nav from "./components/Nav";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import ProfilePage from "./pages/ProfilePage";
-import LogInPage from "./pages/LogInPage";
+import LogInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
 import KampPage from "./pages/KampPage";
 import KampResultatPage from "./pages/KampResultatPage";
 import StevnePage from "./pages/StevnePage";
 import Error from "./pages/ErrorPage";
 import RatingPage from "./pages/RatingPage";
+import { auth } from "./firebase-config";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default function App() {
-  const location = useLocation();
-  const hideNavRoutes = ["/", "/login"];
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth")); // default value comes from localStorage
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -41,27 +43,48 @@ export default function App() {
     );
   }
 
-  return (
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      //user is authenticated / signed in
+      setIsAuth(true); // set isAuth to true
+      localStorage.setItem("isAuth", true); // also, save isAuth in localStorage
+    } else {
+      // user is not authenticated / not signed in
+      setIsAuth(false); // set isAuth to false
+      localStorage.removeItem("isAuth"); // remove isAuth from localStorage
+    }
+  });
+
+  const privateRoutes = (
     <>
-      {!hideNavRoutes.includes(location.pathname) && <Nav />}
-      <main>
-        <Routes>
-          <Route path="/" element={<LogInPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/rating" element={<RatingPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/login" element={<LogInPage />} />
-          <Route path="*" element={<Navigate to="/" />} />
-          <Route path="*" element={<Navigate to="/error" />} />
-          <Route path="/kamp" element={<KampPage />} />
-          <Route path="/kamp/resultat" element={<KampResultatPage />} />
-          <Route path="/stevne" element={<StevnePage />} />
-          <Route path="/error" element={<Error />} />
-          <Route path="/stevne/tilmeld" element={<Error />} />
-        </Routes>
-      </main>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<LogInPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/rating" element={<RatingPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="*" element={<Navigate to="/error" />} />
+        <Route path="/kamp" element={<KampPage />} />
+        <Route path="/kamp/resultat" element={<KampResultatPage />} />
+        <Route path="/stevne" element={<StevnePage />} />
+        <Route path="/error" element={<Error />} />
+        <Route path="/stevne/tilmeld" element={<Error />} />
+      </Routes>
     </>
   );
+
+  // variable holding all public routes without nav bar
+  const publicRoutes = (
+    <Routes>
+      <Route path="/login" element={<LogInPage />} />
+      <Route path="/sign-up" element={<SignUpPage />} />
+      <Route path="*" element={<Navigate to="/sign-in" />} />
+    </Routes>
+  );
+
+  // if user is authenticated, show privateRoutes, else show publicRoutes
+  return <main>{isAuth ? privateRoutes : publicRoutes}</main>;
 }
--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -1,9 +1,101 @@
-import { Fragment } from "react";
-import image from "/image-solid-full.svg";
+import { useState, useEffect, useRef } from "react";
+import { auth } from "../firebase-config";
+import { signOut } from "firebase/auth";
 import pen from "/pen-solid-full.svg";
 import trash from "/trash-solid-full.svg";
 
 export default function ProfileInfo() {
+  const [name, setName] = useState("");
+  const [title, setTitle] = useState("");
+  const [mail, setMail] = useState("");
+  const [image, setImage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fileInputRef = useRef(null);
+
+  const url = `${import.meta.env.VITE_FIREBASE_DATABASE_URL}/users/${
+    auth.currentUser?.uid
+  }.json`; // replace YOUR-FIREBASE-URL with your Firebase URL
+
+  useEffect(() => {
+    async function getUser() {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (data) {
+        setName(data.name);
+        setTitle(data.title);
+        setMail(data.mail);
+        setImage(data.image);
+      }
+      setIsLoading(false);
+    }
+    getUser();
+  }, [url]);
+
+  async function handleSaveUser(event) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    const user = {
+      name,
+      title,
+      mail,
+      image,
+    };
+
+    const response = await fetch(url, {
+      method: "PATCH",
+      body: JSON.stringify(user),
+    });
+
+    if (!response.ok) {
+      setErrorMessage("Sorry, something went wrong. Please try again.");
+    }
+    setIsLoading(false);
+  }
+
+  /**
+   * handleImageChange is called every time the user chooses an image in the file system.
+   * The event is fired by the input file field in the form
+   */
+  async function handleImageChange(event) {
+    const file = event.target.files[0]; // get the first file in the array
+    if (file.size < 500000) {
+      // if file size is below 0.5MB
+      const imageUrl = await uploadImage(file); // call the uploadImage function
+      setImage(imageUrl); // set the image state with the image URL
+      setErrorMessage(""); // reset errorMessage state
+    } else {
+      // if not below 0.5MB display an error message using the errorMessage state
+      setErrorMessage("The image file is too big!");
+    }
+  }
+
+  async function uploadImage(imageFile) {
+    const firebaseProjectId = import.meta.env.VITE_FIREBASE_PROJECT_ID;
+    const url = `https://firebasestorage.googleapis.com/v0/b/${firebaseProjectId}.appspot.com/o/${imageFile.name}`;
+    // POST request to upload image
+    const response = await fetch(url, {
+      method: "POST",
+      body: imageFile,
+      headers: { "Content-Type": imageFile.type },
+    });
+
+    if (!response.ok) {
+      setErrorMessage("Upload image failed"); // set errorMessage state with error message
+      throw new Error("Upload image failed"); // throw an error
+    }
+
+    const imageUrl = `${url}?alt=media`; // get the image URL
+    return imageUrl; // return the image URL
+  }
+
+  function handleSignOut() {
+    signOut(auth); // sign out from firebase/auth
+  }
+
   return (
     <Fragment>
       <div className="profile-info-parent">
@@ -31,13 +123,15 @@ export default function ProfileInfo() {
             <h3>Personlige oplysninger</h3>
           </div>
           <div>
-            <form action="ProfileInfo" className="profile-form">
+            <form className="profile-form" onSubmit={handleSaveUser}>
               <input
                 type="text"
                 className="profile-form-content"
                 id="name"
                 name="name"
                 placeholder="Fornavn"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
               <input
                 type="text"
@@ -45,27 +139,36 @@ export default function ProfileInfo() {
                 id="lastname"
                 name="lastname"
                 placeholder="Efternavn"
+                value={lastname}
+                onChange={(e) => setLastname(e.target.value)}
               />
               <select
                 id="gender"
                 name="gender"
                 className="profile-form-content"
+                value={gender}
+                onChange={(e) => setGender}
               >
-                <option value="women">Kvinde</option>
-                <option value="men">Mand</option>
-                <option value="other">Andet</option>
+                <option value="">Vælg køn</option>
+                <option value="Kvinde">Kvinde</option>
+                <option value="Mand">Mand</option>
+                <option value="Andet">Andet</option>
               </select>
               <input
                 type="date"
                 className="profile-form-content"
                 id="birthday"
-                name="Fødselsdato"
+                name="birthday"
+                value={birthday}
+                onChange={(e) => setBirthday(e.target.value)}
               />
               <input
                 type="text"
                 className="profile-form-content"
                 id="adress"
                 name="adress"
+                value={adress}
+                onChange={(e) => setAdress(e.target.value)}
                 placeholder="Adresse"
               />
               <input
@@ -73,6 +176,8 @@ export default function ProfileInfo() {
                 className="profile-form-content"
                 id="city"
                 name="city"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
                 placeholder="By"
               />
               <input
@@ -80,6 +185,8 @@ export default function ProfileInfo() {
                 className="profile-form-content"
                 id="zip"
                 name="zip"
+                value={zip}
+                onChange={(e) => setZip(e.target.value)}
                 placeholder="Postnummer"
               />
               <input
@@ -87,26 +194,34 @@ export default function ProfileInfo() {
                 className="profile-form-content"
                 id="email"
                 name="email"
-                placeholder="Din email"
+                value={mail}
+                onChange={(e) => setMail(e.target.value)}
+                placeholder="Email"
               />
               <input
                 type="phone"
                 className="profile-form-content"
                 id="phone"
                 name="phone"
-                placeholder="Dit telefonnummer"
+                placeholder="Telefonnummer"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
               />
             </form>
           </div>
           <div className="profile-btns-actions">
             <button
+              type="submit"
               className="profile-btns profile-btns-actions-seperat"
               id="save-btn"
             >
               Gem
             </button>
             <br />
-            <button className="profile-btns profile-btns-actions-seperat">
+            <button
+              className="profile-btns profile-btns-actions-seperat"
+              onClick={handleSignOut}
+            >
               Log ud
             </button>
             <button className="profile-btns profile-btns-actions-seperat">
