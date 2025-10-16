1e0a911847bd5d9c2a6dca3eaed4573403e46ddc
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-10-16 10:10:11 +0200

    feat: update HomePage to fetch match data and display upcoming matches

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,15 +5,49 @@ import NyhedsCard from "../components/NyhedsCard";
 import photo from "/public/img/unsplash-photo.svg";
 import arrow from "/public/arrow-right-black.svg";
 import RatingListe from "../components/RatingListe";
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
+  const [kamp, setKamp] = useState([]);
+
+  useEffect(() => {
+    async function fetchKampe() {
+      const response = await fetch(
+        `${import.meta.env.VITE_FIREBASE_DATABASE_URL}/kampe.json`
+      );
+      const data = await response.json();
+      // from object to array
+      const kampArray = Object.keys(data).map((kampId) => ({
+        id: kampId,
+        ...data[kampId],
+      }));
+
+      setKamp(kampArray);
+    }
+
+    fetchKampe();
+  }, []);
+
+  const iDag = new Date();
+  iDag.setHours(0, 0, 0, 0);
+  //filter kampe fra der har været der
+  const kommendeKampe = kamp.filter((k) => new Date(k.dato) >= iDag);
+  //sort kampe efter dato
+  kommendeKampe.sort((a, b) => new Date(a.dato) - new Date(b.dato));
+
+  kommendeKampe[0]; // den næste kamp
+
+  //if (!nextKamp) return <p>Ingen kommende kampe</p>;
+
   return (
     <section>
       <img src={photo} alt="" />
       <section className="forside">
         <section className="forside-del">
           <h1>Din Næste Kamp</h1>
-          <KampCard />
+          {kommendeKampe.map((kamp) => (
+            <KampCard key={kamp.id} kamp={kamp} />
+          ))}
           <Link className="flex-pil" to="/kamp">
             <p>Se alle kampe</p>
             <img src={arrow} alt="Pil til kamp med id" />
