2e8c43c6879233e38e9699fa07ec3dfeeeb304ff
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-10-16 20:41:42 +0200

    NyhedsCard fixed so no errors acure

--- src/components/NyhedsCard.jsx
+++ src/components/NyhedsCard.jsx
@@ -1,13 +1,13 @@
+import tableTennis from "/public/img/unsplash-photo.svg";
+
 export default function NyhedsCard() {
-    return (
-      <article className="nyheds-card">
-        <div className="img-container">
-          <img src="" alt="nyhedsbillede" />
-        </div>
-        <h2>Nyheds Card</h2>
-        <p>
-          Her kommer nyheds card indholdet
-        </p>
-      </article>
-    );
+  return (
+    <article className="nyheds-card">
+      <div className="img-container">
+        <img src={tableTennis} alt="nyhedsbillede" />
+      </div>
+      <h2>Nyheds Card</h2>
+      <p>Her kommer nyheds card indholdet</p>
+    </article>
+  );
 }
