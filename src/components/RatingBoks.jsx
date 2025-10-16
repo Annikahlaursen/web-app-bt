829d5c628fa503611f3a16d30555c46b8de44021
stinelock <eaa24stlo@students.eaaa.dk>
2025-10-16 13:45:14 +0200

    fix: handle user name assignment in RatingBoks component

--- src/components/RatingBoks.jsx
+++ src/components/RatingBoks.jsx
@@ -1,8 +1,9 @@
 export default function RatingBoks({ user, placering }) {
   //{/*hvordan regnes ens +/- ud?* /}
 
-  const name = `${user.fornavn} ${user.efternavn}`;
+  const name = user.name || `${user.fornavn} ${user.efternavn}`;
   user.name = name;
+
   return (
     <div className="blaa-boks rating-boks-grid">
       <p>{placering}</p>
