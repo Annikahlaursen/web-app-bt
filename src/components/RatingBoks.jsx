c4706db42b277eea4c7031724537e74e23f74e78
stinelock <eaa24stlo@students.eaaa.dk>
2025-10-16 13:49:22 +0200

    fix: display user rating correctly in RatingBoks component

--- src/components/RatingBoks.jsx
+++ src/components/RatingBoks.jsx
@@ -2,13 +2,14 @@ export default function RatingBoks({ user, placering }) {
   //{/*hvordan regnes ens +/- ud?* /}
 
   const name = user.name || `${user.fornavn} ${user.efternavn}`;
-  user.name = name;
+
+  const rating = user.rating || "N/A";
 
   return (
     <div className="blaa-boks rating-boks-grid">
       <p>{placering}</p>
-      <p className="rating-navn">{user.name}</p>
-      <p>{user.rating}</p>
+      <p className="rating-navn">{name}</p>
+      <p>{rating}</p>
       <p>9{/*user.points*/}</p>
     </div>
   );
