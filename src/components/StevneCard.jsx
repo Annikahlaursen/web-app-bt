d5f696eae4cf4172c25bb9910a627e33eeb92481
stinelock <eaa24stlo@students.eaaa.dk>
2025-10-20 11:10:43 +0200

    fix: remove unused fetch logic from StevneCard component

--- src/components/StevneCard.jsx
+++ src/components/StevneCard.jsx
@@ -2,28 +2,8 @@ import location from "/public/location-dot-white.svg";
 import bell from "/public/bell.svg";
 import share from "/public/share.svg";
 import { Link } from "react-router";
-// import { useState, useEffect } from "react";
 
 export default function StevneCard({stevne}) {
-  // const [stevner, setStevner] = useState([]);
-
-  // useEffect(() => {
-  //   async function fetchStevner() {
-  //     const response = await fetch(
-  //       `${import.meta.env.VITE_FIREBASE_DATABASE_URL}/staevner.json`
-  //     );
-  //     const data = await response.json(); // get the data from the response and parse it
-  //     // from object to array
-  //     const stevneArray = Object.keys(data).map((stevneId) => ({
-  //       id: stevneId,
-  //       ...data[stevneId],
-  //     })); // map the data to an array of objects
-
-  //     setStevner(stevneArray); // set the posts state with the postsArray
-  //   }
-
-  //   fetchStevner();
-  // }, []);
 
   return (
     <div className="stevne-card">
