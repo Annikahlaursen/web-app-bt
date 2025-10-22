a4ec0a6c73d268bcd4501f70575d6c817bb23f2f
Annikahlaursen <Annikahlaursen@gmail.com>
2025-10-22 13:08:32 +0200

    feat: update routing to error page in multiple components for improved navigation

--- src/components/NyhedsCard.jsx
+++ src/components/NyhedsCard.jsx
@@ -1,13 +1,16 @@
+import { NavLink } from "react-router";
 import tableTennis from "/public/img/unsplash-photo.svg";
 
 export default function NyhedsCard() {
   return (
-    <article className="nyheds-card">
-      <div className="img-container">
-        <img src={tableTennis} alt="nyhedsbillede" />
-      </div>
-      <h2>Nyheds Card</h2>
-      <p>Her kommer nyheds card indholdet</p>
-    </article>
+    <NavLink to={"/error"}>
+      <article className="nyheds-card">
+        <div className="img-container">
+          <img src={tableTennis} alt="nyhedsbillede" />
+        </div>
+        <h2>Nyheds Card</h2>
+        <p>Her kommer nyheds card indholdet</p>
+      </article>
+    </NavLink>
   );
 }
