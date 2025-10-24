69cda2f1a3763175a4b0ef62dc31e38b466a2cfe
stinelock <eaa24stlo@students.eaaa.dk>
2025-10-24 08:35:55 +0200

    fix: initialize filterCriteria state as an empty object in useFilters hook

--- src/hooks/useFilters.js
+++ src/hooks/useFilters.js
@@ -1,28 +1,28 @@
 import { useState, useEffect } from "react";
 
 export default function useFilters(data, filterFunction) {
-  const [filterCriteria, setFilterCriteria] = useState({
-  });
+  const [filterCriteria, setFilterCriteria] = useState({});
   const [filteredData, setFilteredData] = useState(data);
 
-const applyFilters = () => {
-  const filtered = data.filter((item) => filterFunction(item, filterCriteria));
-  setFilteredData(filtered);
-};
+  const applyFilters = () => {
+    const filtered = data.filter((item) =>
+      filterFunction(item, filterCriteria)
+    );
+    setFilteredData(filtered);
+  };
 
-// Opdaterer filterkriterierne
-const updateFilterCriteria = (key, value) => {
-  setFilterCriteria((prev) => ({
-    ...prev,
-    [key]: value,
-  }));
-};
+  // Opdaterer filterkriterierne
+  const updateFilterCriteria = (key, value) => {
+    setFilterCriteria((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
 
-// Tilpasser filteredData når data eller filterCriteria ændres
-useEffect(() => {
-  applyFilters();
-}, [data, filterCriteria]);
+  // Tilpasser filteredData når data eller filterCriteria ændres
+  useEffect(() => {
+    applyFilters();
+  }, [data, filterCriteria]);
 
-return { filteredData, filterCriteria, updateFilterCriteria };
-
+  return { filteredData, filterCriteria, updateFilterCriteria };
 }
