937c6f9654741ffa0605c71d741b149bad4b0e1f
stinelock <eaa24stlo@students.eaaa.dk>
2025-11-05 11:21:39 +0100

    refactor: remove commented-out code in StevneCard component

--- src/components/StevneCard.jsx
+++ src/components/StevneCard.jsx
@@ -58,34 +58,3 @@ const StevneCard = forwardRef(({ stevne }, ref) => {
 });
 
 export default StevneCard;
-
-// export default function StevneCard({stevne}) {
-
-//   return (
-//     <div className="stevne-card">
-//       <Link to={`/stevne/${stevne}`}>
-//         <div className="kamp-container">
-//           <h3>{stevne?.titel ?? "Ingen titel"}</h3>
-//           <p>{stevne?.dato ?? "Ingen dato tilgængelig"}</p>
-//         </div>
-//         <div className="stevne-container">
-//           <p>Senior, Junior, Ungdom</p>
-//           <div className="location">
-//             <img src={location} alt="Location pin icon" />
-//             <p>{stevne?.lokation ?? "Brabrand hallerne"}</p>
-//           </div>
-//         </div>
-//       </Link>
-//       <div className="kamp-container" id="streg">
-//         <div className="del-notifikationer" id="streg-midt">
-//           <img src={share} alt="Dele ikon" />
-//           <p>Del</p>
-//         </div>
-//         <div className="del-notifikationer">
-//           <img src={bell} alt="Notifikations klokke ikon" />
-//           <p>Notifikationer</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
